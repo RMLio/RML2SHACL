ae806222144b3cfdf624f31687c15b289b40906a
Birte De Smedt <birte.desmedt@student.kuleuven.be>
2020-06-05 17:26:35 +0200

    Finished version cleaning up code

--- RML.py
+++ RML.py
@@ -33,10 +33,10 @@ class RML:
             for stmt in self.graph:
                 pprint.pprint(stmt)
     def createGraph(self,number, letter,typeInputFile):
-        fileReadObject = FilesGitHub()
-        filename = fileReadObject.getFile(number,letter,typeInputFile,fileReadObject.Mappingfile)
+        fileReadObj = FilesGitHub()
+        filename = fileReadObj.getFile(number,letter,typeInputFile,fileReadObj.Mappingfile)
         self.graph.parse(filename,format=rdflib.util.guess_format(filename)) 
-
+        #self.printGraph(1)
     def removeBlankNodesMultipleMaps(self):
         #loop over all the Triple Maps in the RML input file
         for sTM,pTM,oTM in self.graph.triples((None,None,self.r2rmlNS.TriplesMap)):
@@ -50,16 +50,18 @@ class RML:
             tel=0
             #inside one Triple Map we doe loops over:
             for s,p,o in self.graph.triples((sTM,None,None)):
-                #if s==sTM:
                     #the triples belonging to the Logical Source
                     if p == self.rmlNS.logicalSource:
-                        for s2,p2,o2 in self.graph.triples((o,None,None)): #searching for same Blank Node
+                        for s2,p2,o2 in self.graph.triples((o,None,None)): 
+                        #searching for same Blank Node
                             graphlogicalSource.add((p,p2,o2)) #add logical source info
                         graphHelp["LS"] = graphlogicalSource
                     #the triples belonging to the Subject Map
                     if p == self.sSM:
-                        for s2,p2,o2 in self.graph.triples((o,None,None)): #searching for same Blank Node
-                            graphsubjectMap.add((p,p2,o2)) #add subject Map  info
+                        for s2,p2,o2 in self.graph.triples((o,None,None)): 
+                        #searching for same Blank Node
+                            graphsubjectMap.add((p,p2,o2))
+                         #add subject Map  info
                         graphHelp["SM"] = graphsubjectMap
                     #the multiple triples that are PredicateObject Maps
                     if p == self.sPOM:
@@ -68,33 +70,40 @@ class RML:
                         for s2,p2,o2 in self.graph.triples((o,None,None)): #searching for same Blank Node
                             if p2 == self.r2rmlNS.predicateMap:
                                 for s3,p3,o3 in self.graph.triples((o2,self.pCons,None)):
-                                    #we make the "rr:predicateMap rr:constant o" triple to sthe shurtcut "rr:PredicateObjectMap rr:predicate o2" 
-                                   graphPredicatObjectMap.add((p,self.pPred,o3))       #add the predicateobjectMap with the constant transformed into rr:predicate instead of constant
+#we make the "rr:predicateMap rr:constant o" triple to sthe shurtcut "rr:PredicateObjectMap rr:predicate o2" 
+                                   graphPredicatObjectMap.add((p,self.pPred,o3))      
+ #add the predicateobjectMap with the constant transformed into rr:predicate instead of constant
                             else:
-                                graphPredicatObjectMap.add((p,p2,o2))       #add the predicateobjectMap
-                        #searching for which objectMap belongs to this PredicateObjectMap 
+                                graphPredicatObjectMap.add((p,p2,o2))       
+                            #add the predicateobjectMap
+#searching for which objectMap belongs to this PredicateObjectMap 
                         for s2,p2,o2 in graphPredicatObjectMap.triples((p,self.oM,None)):
                             for s3,p3,o3 in self.graph.triples((o2,None,None)):
-                                graphPredicatObjectMap.add((p2,p3,o3))      #add the objectMap beloning to the predicateobjectMap added in previous loop
-                            graphPredicatObjectMap.remove((s2,p2,o2)) #remove something with a blanknode in that we added too much
-                        # if we don't have an rr:ObjectMap but an rr:object (as part of rr:predicateMap as an predicate) we will write this as rr:ObjectMap rr:constant (object that belonged to the rr:object)
+                                graphPredicatObjectMap.add((p2,p3,o3))     
+#add the objectMap beloning to the predicateobjectMap added in previous loop
+                            graphPredicatObjectMap.remove((s2,p2,o2)) 
+#remove something with a blanknode in that we added too much
+# if we don't have an rr:ObjectMap but an rr:object (as part of rr:predicateMap as an predicate) 
+# we will write this as rr:ObjectMap rr:constant (object that belonged to the rr:object)
                         for s2,p2,o2 in graphPredicatObjectMap.triples((p,self.obj,None)):
-                            #graphPredicatObjectMap.add((s2,p2,o2))      #add the object beloning to the predicateobjectMap added in previous loop'''
+ #graphPredicatObjectMap.add((s2,p2,o2))      
+ # #add the object beloning to the predicateobjectMap added in previous loop
                             graphPredicatObjectMap.add((self.oM,self.pCons,o2))
-                            graphPredicatObjectMap.remove((s2,p2,o2)) #remove the "rr:predicateMap rr:object o2" triple from the graph because it gets added in loop for objectMap
+                            graphPredicatObjectMap.remove((s2,p2,o2))
+ #remove the "rr:predicateMap rr:object o2" triple from the graph because it gets added in loop for objectMap
                         #loop to find any possible RefObjectMaps
                         for sROM,pROM,oROM in self.graph.triples((None,None,self.r2rmlNS.RefObjectMap)):
-                            #if we find one we see if it belongs to the ObjectMap we are working with now
+#if we find one we see if it belongs to the ObjectMap we are working with now
                             for s3,p3,o3 in self.graph.triples((p,self.oM,sROM)):
-                                #if this is the fact we search inside the RefObjectMap (sROM) for the value of rr:parentTriplesMap
+#if this is the fact we search inside the RefObjectMap (sROM) for the value of rr:parentTriplesMap
                                 for s4, p4,o4 in self.graph.triples((sROM,self.r2rmlNS.parentTriplesMap,None)):
-                                    graphPredicatObjectMap.add((self.oM,self.r2rmlNS.parentTriplesMap,o4)) #add the parentTriplesMap to the ObjectMap
+                                    graphPredicatObjectMap.add((self.oM,self.r2rmlNS.parentTriplesMap,o4))
+#add the parentTriplesMap to the ObjectMap
 
                         graphHelp["POM"+str(tel)] = graphPredicatObjectMap
                         tel = tel +1
             self.graphs.append(graphHelp)
     def printDictionary(self, keuze):
-        #print(self.graphs)
         for graphHelp in self.graphs:
             if keuze == 1:
                 for g in graphHelp["TM"]:
@@ -107,8 +116,10 @@ class RML:
                     print(g)
             if keuze == 4:
                 length = len(graphHelp)-3
-                #Because the dictionary inside graphHelp has first 'TM', 'LM' and 'SM' as keys we do the length of the dictionary minus 3
-                #this way we can use this newly calculated length for the indexes used for the possible multiple PredicateObjectsMaps (POM)
+#Because the dictionary inside graphHelp has first 'TM', 'LM' and 'SM' as keys
+# we do the length of the dictionary minus 3
+#this way we can use this newly calculated length 
+#for the indexes used for the possible multiple PredicateObjectsMaps (POM)
                 for i in range(length):
                     print("new POM" + str(i))
                     for g in graphHelp["POM"+str(i)]:
@@ -125,7 +136,8 @@ class RML:
             
     def testmain(self):
         self.createGraph(7,'b',FilesGitHub.CSV)
-        #self.removeBlankNodesMultipleMaps()
+        self.removeBlankNodesMultipleMaps()
+        self.printDictionary(5)
 if __name__ == '__main__':
     Rml = RML()
     Rml.testmain()
