57cf345259140666da984c739916216cd577ee81
Sitt Min Oo <sittminoo1995145@gmail.com>
2021-10-18 15:58:18 +0200

    started refactoring github test cases downloading script

--- src/FilesGitHub.py
+++ src/FilesGitHub.py
@@ -13,9 +13,38 @@ class FilesGitHub:
     FileTypes = [CSV, XML, mySql, Postgre, js, sparql, sqlserver]
     Mappingfile = '/mapping.ttl'
     outputRdfFile = '/output.nq'
+    url = "https://api.github.com/repos/kg-construct/rml-test-cases/git/trees/master"
 
     def __init__(self):
         pass
+
+    def getTestCasesFolder(self)-> str: 
+        r = requests.get(self.url)
+        r.raise_for_status()  # test if there are any errors when trying to read the testcases
+
+        for trunk in r.json()["tree"]: 
+            if trunk["path"] == "test-cases":
+                return trunk["url"]
+
+        return ""
+
+    def downloadAllTestCases(self): 
+        root_folder =  self.getTestCasesFolder() 
+
+        r = requests.get(root_folder)
+        r.raise_for_status()
+        r = r.json()
+        test_folders = [x for x in r["tree"] if x["type"] == "tree" ] 
+
+        for folder in  test_folders: 
+            folder_name = folder["path"]
+            r = requests.get(folder["url"]) 
+            test_mapping_file_url = [x for x in r.json()["tree"] if x["path"] == "mapping.ttl"][0]["url"] 
+
+            r = requests.get(test_mapping_file_url)
+
+            print(r)
+            return
 
     def getFile(self, number, letter, typeFile, fileNeeded, save_mapping=True, mapping_dir="mapping/"):
         # This function makes it possible to get the RML input files with the matching RDF output file from GitHub
